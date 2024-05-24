9b592c8d132b13babf8b76c2be33235f4ca23951
TheRiverElder <765375399@qq.com>
2024-05-25 03:25:42 +0800

    修复了稍后再看列表加载是啊比的问题：服务器返回的json.data.list=>page.cid超过int范围

--- src/main/java/top/riverelder/android/bilibilifucker/BilibiliFucker.java
+++ src/main/java/top/riverelder/android/bilibilifucker/BilibiliFucker.java
@@ -55,66 +55,68 @@ public class BilibiliFucker implements IXposedHookLoadPackage {
         ClassLoader classLoader = lpparam.classLoader;
 
         // 解除长按快进
-        try {
-            findAndHookMethod("tv.danmaku.biliplayerimpl.gesture.GestureService", lpparam.classLoader, "f6", DO_NOTHING);
-            log("Hook succeeded: Preventing Function: LongPressToFastForward");
-        } catch (Exception e) {
-            log("Hook failed: Preventing Function: LongPressToFastForward");
-            log("message: " + e.getMessage());
-        }
+//        try {
+//            findAndHookMethod("tv.danmaku.biliplayerimpl.gesture.GestureService", lpparam.classLoader, "f6", DO_NOTHING);
+//            log("Hook succeeded: Preventing Function: LongPressToFastForward");
+//        } catch (Exception e) {
+//            log("Hook failed: Preventing Function: LongPressToFastForward");
+//            log("message: " + e.getMessage());
+//        }
 
         // 解除评论区关键字蓝字
-        try {
-            findAndHookMethod(
-                    "com.bilibili.app.comm.comment2.model.rpc.CommentRpcKt",
-                    lpparam.classLoader,
-                    "p",
-                    "com.bapis.bilibili.main.community.reply.v1.ReplyInfo",
-                    new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//                            log("comment result = " + param.getResult());
-                        }
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            Object comment = param.getResult();
-//                            log("Someone is getting BiliComment with fields: ");
-//                            printBean(comment);
-                            try {
-                                setDescendantField(comment, new String[]{ "mContent", "jumpUrls" }, null);
-                                param.setResult(comment);
-                            } catch (Exception ignored) { }
-
-//                            log("Someone is constructing BiliComment with fields: ");
-//                            printFields(param.thisObject);
-//                            log("Someone received BiliCommentDetail: ");
-//                            log("comment result = " + param.getResult());
-                        }
-                    });
-            log("Hook succeeded: Preventing Function: KeyWorkLink");
-        } catch (Exception e) {
-            log("Hook failed: Preventing Function: KeyWorkLink");
-            log("message: " + e.getMessage());
-        }
+//        try {
+//            findAndHookMethod(
+//                    "com.bilibili.app.comm.comment2.model.rpc.CommentRpcKt",
+//                    lpparam.classLoader,
+//                    "p",
+//                    "com.bapis.bilibili.main.community.reply.v1.ReplyInfo",
+//                    new XC_MethodHook() {
+//                        @Override
+//                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+////                            log("comment result = " + param.getResult());
+//                        }
+//
+//                        @Override
+//                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                            Object comment = param.getResult();
+////                            log("Someone is getting BiliComment with fields: ");
+////                            printBean(comment);
+//                            try {
+//                                setDescendantField(comment, new String[]{"mContent", "jumpUrls"}, null);
+//                                param.setResult(comment);
+//                            } catch (Exception ignored) {
+//                            }
+//
+////                            log("Someone is constructing BiliComment with fields: ");
+////                            printFields(param.thisObject);
+////                            log("Someone received BiliCommentDetail: ");
+////                            log("comment result = " + param.getResult());
+//                        }
+//                    });
+//            log("Hook succeeded: Preventing Function: KeyWorkLink");
+//        } catch (Exception e) {
+//            log("Hook failed: Preventing Function: KeyWorkLink");
+//            log("message: " + e.getMessage());
+//        }
 
         // 禁止在点击UP头像时转跳到直播间
-        try {
-//            "tv.danmaku.bili.videopage.common.widget.view.VerifyAvatarFrameLayout",
-            findAndHookMethod(
-                    "tv.danmaku.bili.videopage.data.view.model.OwnerExt",
-                    lpparam.classLoader,
-                    "hasLive",
-                    new XC_MethodReplacement() {
-                        @Override
-                        public Object replaceHookedMethod(MethodHookParam param) {
-                            return false;
-                        }
-                    });
-            log("Hook succeeded: Preventing Function: VideoPageAuthorAvatarToLiveRoom");
-        } catch (Exception e) {
-            log("Hook failed: Preventing Function: VideoPageAuthorAvatarToLiveRoom");
-            log("message: " + e.getMessage());
-        }
+//        try {
+////            "tv.danmaku.bili.videopage.common.widget.view.VerifyAvatarFrameLayout",
+//            findAndHookMethod(
+//                    "tv.danmaku.bili.videopage.data.view.model.OwnerExt",
+//                    lpparam.classLoader,
+//                    "hasLive",
+//                    new XC_MethodReplacement() {
+//                        @Override
+//                        public Object replaceHookedMethod(MethodHookParam param) {
+//                            return false;
+//                        }
+//                    });
+//            log("Hook succeeded: Preventing Function: VideoPageAuthorAvatarToLiveRoom");
+//        } catch (Exception e) {
+//            log("Hook failed: Preventing Function: VideoPageAuthorAvatarToLiveRoom");
+//            log("message: " + e.getMessage());
+//        }
 
         // Debug用的view绑定
 //        hookViewClickEvent();
@@ -192,25 +194,94 @@ public class BilibiliFucker implements IXposedHookLoadPackage {
 //                printBean(param.args[0]);
 //            }
 //        });
+//        try {
+//            XposedHelpers.findAndHookMethod("com.bilibili.pegasus.promo.index.IndexFeedFragmentV2", classLoader, "Ax", "com.bilibili.pegasus.api.modelv2.PegasusFeedResponse", new XC_MethodHook() {
+//                @Override
+//                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                    log("!!! ");
+//                    Utils.BufferedResult bufferedResult = tryGetDescendantField(param.args[0], "config");
+//                    if (!bufferedResult.succeeded) return;
+//                    Object config = bufferedResult.result;
+//                    boolean autoRefreshTimeByActiveResult = trySetDescendantField(config, new String[] { "autoRefreshTimeByActive" }, 0L);
+//                    boolean autoRefreshTimeByAppearResult = trySetDescendantField(config, new String[] { "autoRefreshTimeByAppear" }, 0L);
+//    //                log("autoRefreshTimeByActiveResult = " + autoRefreshTimeByActiveResult);
+//    //                log("autoRefreshTimeByAppearResult = " + autoRefreshTimeByAppearResult);
+//    //                log("aaaaaaaa ");
+//    //                printBean(config);
+//                }
+//            });
+//            log("Hook succeeded: Preventing Function: AutoRefreshRecommendVideoList");
+//        } catch (Exception e) {
+//            log("Hook failed: Preventing Function: AutoRefreshRecommendVideoList");
+//            log("message: " + e.getMessage());
+//        }
+
+        // 修复稍后再看列表
         try {
-            XposedHelpers.findAndHookMethod("com.bilibili.pegasus.promo.index.IndexFeedFragmentV2", classLoader, "Ax", "com.bilibili.pegasus.api.modelv2.PegasusFeedResponse", new XC_MethodHook() {
+            log("Hook Start: FixWatchLater");
+
+            Class<?> FastJsonClass = classLoader.loadClass("com.alibaba.fastjson.JSON");
+            ;
+            Class<?> OkHttpResponseClass = classLoader.loadClass("okhttp3.e0");
+            Class<?> FeatureClass = classLoader.loadClass("com.alibaba.fastjson.parser.Feature");
+            Class<?> WatchLaterListConverterClass = classLoader.loadClass("com.bilibili.okretro.converter.b");
+
+            XposedHelpers.findAndHookMethod("com.bilibili.okretro.converter.b", classLoader, "convert", java.lang.Object.class, new XC_MethodReplacement() {
                 @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    log("!!! ");
-                    Utils.BufferedResult bufferedResult = tryGetDescendantField(param.args[0], "config");
-                    if (!bufferedResult.succeeded) return;
-                    Object config = bufferedResult.result;
-                    boolean autoRefreshTimeByActiveResult = trySetDescendantField(config, new String[] { "autoRefreshTimeByActive" }, 0L);
-                    boolean autoRefreshTimeByAppearResult = trySetDescendantField(config, new String[] { "autoRefreshTimeByAppear" }, 0L);
-    //                log("autoRefreshTimeByActiveResult = " + autoRefreshTimeByActiveResult);
-    //                log("autoRefreshTimeByAppearResult = " + autoRefreshTimeByAppearResult);
-    //                log("aaaaaaaa ");
-    //                printBean(config);
+                protected Object replaceHookedMethod(MethodHookParam methodHookParam) throws Throwable {
+                    try {
+                        // 替换其他方法，XP主动调用 方法
+                        String rawResponseString = (String) XposedHelpers.callMethod(methodHookParam.args[0], "m");
+                        String responseString = rawResponseString;
+                        {
+                            Object json = XposedHelpers.callStaticMethod(FastJsonClass, "parseObject", rawResponseString);
+                            Object data = XposedHelpers.callMethod(json, "getJSONObject", "data");
+                            if (data != null) {
+                                Object list = XposedHelpers.callMethod(data, "getJSONArray", "list");
+                                if (list != null) {
+                                    int listSize = (Integer) XposedHelpers.callMethod(list, "size");
+                                    boolean changed = false;
+                                    for (int i = 0; i < listSize; i++) {
+                                        Object item = XposedHelpers.callMethod(list, "getJSONObject", Integer.valueOf(i));
+                                        Object page = XposedHelpers.callMethod(item, "getJSONObject", "page");
+                                        if (page != null) {
+                                            long cid = (Long) XposedHelpers.callMethod(page, "getLong", "cid");
+                                            if (cid > Integer.MAX_VALUE) {
+                                                int newCid = (int) (cid & Integer.MAX_VALUE);
+                                                XposedHelpers.callMethod(page, "put", "cid", newCid);
+                                                changed = true;
+                                            }
+                                        }
+                                    }
+
+                                    if (changed) {
+                                        responseString = (String) XposedHelpers.callStaticMethod(FastJsonClass, "toJSONString", json);
+                                    }
+                                }
+                            }
+                        }
+
+                        log("responseString.length = " + responseString.length());
+                        Object beanType = XposedHelpers.getObjectField(methodHookParam.thisObject, "a");
+                        log("beanType = " + beanType);
+                        int FEATURE_FLAGS = XposedHelpers.getStaticIntField(WatchLaterListConverterClass, "b");
+                        log("FEATURE_FLAGS = " + FEATURE_FLAGS);
+                        Object[] features = (Object[]) Array.newInstance(FeatureClass, 1);
+                        features[0] = XposedHelpers.getStaticObjectField(FeatureClass, "AutoCloseSource");
+                        log("features = " + Arrays.toString(features));
+
+                        Object result = XposedHelpers.callStaticMethod(FastJsonClass, "parseObject", responseString, beanType, FEATURE_FLAGS, features);
+
+                        return result;
+                    } catch (Throwable e) {
+                        return XposedBridge.invokeOriginalMethod (methodHookParam.method, methodHookParam.thisObject, methodHookParam.args);
+                    }
                 }
             });
-            log("Hook succeeded: Preventing Function: AutoRefreshRecommendVideoList");
+
+            log("Hook succeeded: FixWatchLater");
         } catch (Exception e) {
-            log("Hook failed: Preventing Function: AutoRefreshRecommendVideoList");
+            log("Hook failed: FixWatchLater");
             log("message: " + e.getMessage());
         }
 
